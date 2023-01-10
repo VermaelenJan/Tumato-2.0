84248d8be9cb86db70e8b2a58afa336928da1663
Jan Vermaelen <jan.vermaelen@cs.kuleuven.be>
2023-01-10 15:36:35 +0100

    costs become durations, for now

--- policy_generator/src/main/java/ai/ivex/policygenerator/cpmodel/CpModel.java
+++ policy_generator/src/main/java/ai/ivex/policygenerator/cpmodel/CpModel.java
@@ -8,7 +8,6 @@ import ai.ivex.policygenerator.protobufspec.predicates.Predicate;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.constraints.Constraint;
-import org.chocosolver.solver.variables.BoolVar;
 import org.chocosolver.solver.variables.IntVar;
 
 import java.util.List;
